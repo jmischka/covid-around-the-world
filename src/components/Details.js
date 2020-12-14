71f0742bdab1f4f4109e8775fc1cbcbeeb15374d
Jason Mischka <jasonmischka@Jasons-MBP.fios-router.home>
2020-12-14 09:42:02 -0500

    updated copy edits

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Graphs from './Graphs';
 
+const today = new Date();
+const date = new Intl.DateTimeFormat('en', { month: 'long', day: '2-digit', year: 'numeric' }).format(today);
+
 class Details extends Component {
   render() {
     const {triggered} = this.props;
@@ -14,19 +17,19 @@ class Details extends Component {
             </div> 
             <div className="details-content">
               <div className="details-totalConfirmed">
-                <span className="stat-label">Total Confirmed Cases Countrywide:</span>
+                <span className="stat-label">Total Confirmed Cases Nationwide:</span>
                 <span className="stat-number">{this.props.countryDetails.TotalConfirmed}</span>
               </div>
               <div className="details-totalDeath">
-                  <span className="stat-label">Total Deaths Countrywide:</span>
+                  <span className="stat-label">Total Deaths Nationwide:</span>
                   <span className="stat-number">{this.props.countryDetails.TotalDeaths}</span>
               </div>
               <div className="details-newConfirmed">
-                  <span className="stat-label">New Confirmed Cases Countrywide:</span>
+                  <span className="stat-label">New Confirmed Cases Nationwide:<br /><span>{date}</span></span>
                   <span className="stat-number">{this.props.countryDetails.NewConfirmed}</span>
               </div>
               <div className="details-newDeath">
-                  <span className="stat-label">New Deaths Countrywide:</span>
+                  <span className="stat-label">New Deaths Nationwide:<br /><span>{date}</span></span>
                   <span className="stat-number">{this.props.countryDetails.NewDeaths}</span>
               </div>
 
